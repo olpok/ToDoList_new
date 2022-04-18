d78953d8a9fd1ee9f2ad3886ce5c5bce064b1963
olga <olgafff@free.fr>
2022-04-18 21:27:29 +0200

    Change prefix name route 'tasks' instead of 'task'

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-#[Route('/task')]
+#[Route('/tasks')]
 class TaskController extends AbstractController
 {
     #[Route('/', name: 'task_index', methods: ['GET'])]
