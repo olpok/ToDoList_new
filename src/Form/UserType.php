7d2d3622d08f7aa87c5dada422d87667cd32322b
olga <olgafff@free.fr>
2022-05-10 19:39:46 +0200

    Add ChoiceTypeField for roles in User entity

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -20,7 +20,12 @@ class UserType extends AbstractType
         $builder
             ->add('username', TextType::class, ['label' => "Nom d'utilisateur"])
             ->add('email', EmailType::class, ['label' => 'Adresse email'])
-            ->add('roles')
+            ->add('roles', ChoiceType::class, [
+                'choices'  => [
+                    'ROLE_USER' => 'ROLE_USER',
+                    'ROLE_ADMIN' => 'ROLE_ADMIN'
+                ]
+            ])
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'invalid_message' => 'Les deux mots de passe doivent correspondre.',
